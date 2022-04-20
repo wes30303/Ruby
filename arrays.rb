b968070c53eb540faa4a0d6f67b20f762958de88
Wesley Miranda <wesleywes42@gmail.com>
2022-04-20 10:32:25 -0600

    3 new arrays

--- arrays.rb
+++ arrays.rb
@@ -1,10 +1,9 @@
 Student = ["josh", "dave", "alex", "bob"]
 score = [7, 10, 19, 10]
 bank = [71.30, 50.50, 100.50, 75.50]
-Happy = True
-Dog = True
-Paid = false
-Valid = flase
+Happy = [True, True, false, false]
+
+
 # Index position 3 for students is bob. Index position 0 in score is 7
 
 
@@ -19,3 +18,11 @@ bank.shift
 
 #this array will list the arrays
 Happy.lengh
+
+
+score.last
+#will call last element
+score.first
+#will call first element
+score.take(3)
+#will call frist 3 elements
